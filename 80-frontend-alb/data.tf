195adbbcca3b7cea7dab052e3126a09afb50e6ea
Naga <pavantonystark99@gmail.com>
2025-12-25 22:16:15 +0530

    added chnages to ingress

--- 80-frontend-alb/data.tf
+++ 80-frontend-alb/data.tf
@@ -4,9 +4,9 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 data "aws_ssm_parameter" "bastion_id" {
   name = "/${var.project}/${var.environment}/bastion_sg-id"
 }
-data "aws_ssm_parameter" "backend-alb_sg-id" {
-  name = "/${var.project}/${var.environment}/backend_alb_sg-id" #/roboshop/dev/backend-alb_sg-id
-}
+# data "aws_ssm_parameter" "backend-alb_sg-id" {
+#   name = "/${var.project}/${var.environment}/backend_alb_sg-id" #/roboshop/dev/backend-alb_sg-id
+# }
 # data "aws_ssm_parameter" "backend-alb_sg-id" {
 #   name = "/${var.project}/${var.environment}/backend-alb_sg-id" #/roboshop/dev/backend-alb_sg-id
 # }
@@ -15,9 +15,9 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
-data "aws_ssm_parameter" "frontend-alb_sg-id" {
+data "aws_ssm_parameter" "ingress_alb_sg_id" {
   name = "/${var.project}/${var.environment}/frontend_alb_sg-id" #/roboshop/dev/backend-alb_sg-id
 }
-data "aws_ssm_parameter" "frontend_alb_certificate_arn" {
-  name = "/${var.project}/${var.environment}/frontend_alb_certificate_arn"
+data "aws_ssm_parameter" "ingress_alb_certificate_arn" {
+  name = "/${var.project}/${var.environment}/ingress_alb_certificate_arn"
 }
--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -1,8 +1,8 @@
-resource "aws_lb" "frontend_alb" {
-  name               = "${local.common_name_suffix}-Frontend-ALB"
+resource "aws_lb" "ingress_alb" {
+  name               = "${local.common_name_suffix}-ingress-ALB"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_ssm_parameter.frontend-alb_sg-id.value]
+  security_groups    = [data.aws_ssm_parameter.ingress_alb_sg_id.value]
   subnets            = local.public_subnets_array
 
   enable_deletion_protection = false #prevents accidental deletion from UI or anywhere
@@ -13,23 +13,23 @@ resource "aws_lb" "frontend_alb" {
     var.backend_tags,
     local.common_tags,
     {
-      Name = "${local.common_name_suffix}-Frontend-ALB-Resource"
+      Name = "${local.common_name_suffix}-ingress-ALB-Resource"
     }
   )
 }
 
-resource "aws_lb_listener" "frontend_alb" {
-  load_balancer_arn = aws_lb.frontend_alb.arn
+resource "aws_lb_listener" "ingress_alb" {
+  load_balancer_arn = aws_lb.ingress_alb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
-  certificate_arn   = data.aws_ssm_parameter.frontend_alb_certificate_arn.value
+  certificate_arn   = data.aws_ssm_parameter.ingress_alb_certificate_arn.value
 
   default_action {
     type = "fixed-response"
     fixed_response {
       content_type = "text/plain"
-      message_body = "<h1> I am from HTTPS frontend-alb</h1>"
+      message_body = "<h1> I am from HTTPS ingress-alb</h1>"
       status_code  = "200"
     }
   }
@@ -38,16 +38,17 @@ resource "aws_lb_listener" "frontend_alb" {
 
 #Creating Route 53 reord for frontend alb
 
-resource "aws_route53_record" "frontend_alb" {
+resource "aws_route53_record" "ingress_alb" {
   zone_id         = var.zone_id
-  name            = "roboshop-${var.environment}.${var.zone_name}"
+  name            = "*.${var.zone_name}"
+  # name            = "roboshop-${var.environment}.${var.zone_name}"
   type            = "A"
   allow_overwrite = true
 
   alias {
     #these are realted to alb not our domain details
-    name                   = aws_lb.frontend_alb.dns_name
-    zone_id                = aws_lb.frontend_alb.zone_id
+    name                   = aws_lb.ingress_alb.dns_name
+    zone_id                = aws_lb.ingress_alb.zone_id
     evaluate_target_health = true
   }
 }
